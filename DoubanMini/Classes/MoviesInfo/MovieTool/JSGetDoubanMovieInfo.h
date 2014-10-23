e6cc29727eafaee8fd0f73186da3a9f56e6219e0
Jsonmess <jsonmess@gmail.com>
2014-10-23 09:58:31 +0800

    初始化电影模块

---
+++ DoubanMini/Classes/MoviesInfo/MovieTool/JSGetDoubanMovieInfo.h
@@ -0,0 +1,13 @@
+//
+//  JSGetDoubanMovieInfo.h
+//  DoubanMini
+//
+//  Created by json on 14/10/22.
+//  Copyright (c) 2014年 jsonmess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSGetDoubanMovieInfo : NSObject
+
+@end
