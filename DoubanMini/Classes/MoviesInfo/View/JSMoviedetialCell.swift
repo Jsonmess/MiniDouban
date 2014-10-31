4b5d733d1643e79d0a15f9e402635ad5e1906cf6
jsonmess <jsonmess@gmail.com>
2014-10-31 21:13:51 +0800

    修正更新视图bug，将更新数据信息放入主线程中，下载电影海报的耗时操作放入全局队列中

--- DoubanMini/Classes/MoviesInfo/View/JSMoviedetialCell.swift
+++ DoubanMini/Classes/MoviesInfo/View/JSMoviedetialCell.swift
@@ -45,17 +45,19 @@ public class JSMoviedetialCell: UICollectionViewCell {
     //MARK:传入电影名称、电影海报、电影评分
     public func setMoviedetialCellBy(MovieName name:NSString,MoviePic picurl:NSString, MovieRating rating:Float)
     {
-        var  queue: dispatch_queue_attr_t=dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-        self.movieName.text=name;
-        self.ratingview?.showStarbyRatingValueWith(RatingValue: rating);
         //电影海报--使用多线程主线程更新视图
+        var  queue: dispatch_queue_attr_t=dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         dispatch_async(queue, { () -> Void in
-               var imageview:UIImageView=JSLoadUrlImage.getimageWithUrl(picurl);
+            var imageview:UIImageView=JSLoadUrlImage.getimageWithUrl(picurl);
             //主线程更新
             dispatch_sync(dispatch_get_main_queue(), { () -> Void in
-              self.moviePicture.image=imageview.image;
+                
+                self.movieScoreValue.text=NSString(format: "%.1f", rating);
+                self.movieName.text=name;
+                self.ratingview?.showStarbyRatingValueWith(RatingValue: rating);
+                self.moviePicture.image=imageview.image;
             });
         });
-        self.movieScoreValue.text=NSString(format: "%.1f", rating);
+        
     }
 }
